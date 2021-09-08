e1160a4cbcef4991a7002f9bc9ae2f61dd0ffb65
Tony Bogdanov <tonybogdanov@gmail.com>
2021-09-08 18:52:36 +0300

    feat: ocimp.urify

---
+++ src/tasks/urify.js
@@ -0,0 +1,16 @@
+/**
+ * Serializes the specified file and returns it as a data URI.
+ *
+ * @param file
+ * @returns {Promise<unknown>}
+ */
+export default file => new Promise( ( resolve, reject ) => {
+
+    const reader = new FileReader();
+
+    reader.addEventListener( 'load', () => resolve( reader.result ) );
+    reader.addEventListener( 'error', reject );
+
+    reader.readAsDataURL( file );
+
+} );
---
+++ tests/urify.js
@@ -0,0 +1,23 @@
+import { runBackend } from 'worker-relay';
+
+async function test( assert, ext, mime ) {
+
+    const input = await ( await fetch( `/a.${ ext }` ) ).blob();
+    const imageData = await runBackend( 'ocimp.decode', input );
+
+    const output = await runBackend( 'ocimp.encode', imageData, mime );
+    const file = await runBackend( 'ocimp.pack', output, 'file', 0 );
+
+    const uri = await runBackend( 'ocimp.urify', file );
+
+    assert.isString( uri );
+    assert.equal( `data:${ mime };base64,`, uri.substr( 0, mime.length + 13 ) );
+
+}
+
+export default {
+
+    'Should uri-fy blob(jpeg).': async assert => test( assert, 'jpg', 'image/jpeg' ),
+    'Should uri-fy blob(png).': async assert => test( assert, 'png', 'image/png' ),
+
+};

d74d4637031f4fbb4731e2cf3592b40181803d63
Tony Bogdanov <tonybogdanov@gmail.com>
2022-03-18 17:06:00 +0200

    fix: vite compatible dynamic imports

--- src/resolver.js
+++ src/resolver.js
@@ -1,24 +1,21 @@
 let tasks = undefined;
 
 export default function resolver( name ) {
-
     if ( 'undefined' === typeof tasks ) {
+        const proxy = fn => async ( ... args ) => ( await fn() ).default( ... args );
 
-        tasks = {};
-        const context = require.context( './tasks', true, /\.js$/, 'lazy' );
-
-        context.keys().forEach( path => {
-
-            const rel = path.replace( /^[.\/]+/, '' );
-            const name = rel.replace( /\.js$/, '' );
-
-            tasks[ `ocimp.${ name }` ] = async ( ... args ) =>
-                ( await import( /* @vite-ignore */ `./tasks/${ rel }` ) ).default( ... args );
-
-        } );
-
+        tasks = {
+            'ocimp.contain': proxy( () => import( './tasks/contain' ) ),
+            'ocimp.cover': proxy( () => import( './tasks/cover' ) ),
+            'ocimp.crop': proxy( () => import( './tasks/crop' ) ),
+            'ocimp.decode': proxy( () => import( './tasks/decode' ) ),
+            'ocimp.encode': proxy( () => import( './tasks/encode' ) ),
+            'ocimp.fit': proxy( () => import( './tasks/fit' ) ),
+            'ocimp.pack': proxy( () => import( './tasks/pack' ) ),
+            'ocimp.resize': proxy( () => import( './tasks/resize' ) ),
+            'ocimp.urify': proxy( () => import( './tasks/urify' ) ),
+        };
     }
 
     return tasks.hasOwnProperty( name ) ? tasks[ name ] : null;
-
 };
--- test.js
+++ test.js
@@ -57,76 +57,59 @@ function batch( queue ) {
 }
 
 async function capabilities() {
-
     console.log( 'Resolving browser capabilities.' );
-    const caps = await getCapabilities( {
+    const candidates = { min: {}, max: {} };
 
+    const caps = await getCapabilities( {
         username: process.env.BROWSERSTACK_USERNAME,
         accessKey: process.env.BROWSERSTACK_ACCESS_KEY,
-
     } );
 
-    const candidates = { min: {}, max: {} };
     for ( const cap of caps ) {
-
         if ( ! candidates.min.hasOwnProperty( cap.browserName ) ||
             0 < compare( candidates.min[ cap.browserName ].browserVersion, cap.browserVersion ) ) {
-
             candidates.min[ cap.browserName ] = cap;
-
         }
 
         if ( ! candidates.max.hasOwnProperty( cap.browserName ) ||
             0 > compare( candidates.max[ cap.browserName ].browserVersion, cap.browserVersion ) ) {
-
             candidates.max[ cap.browserName ] = cap;
-
         }
-
     }
 
     return Object.values( candidates.min ).concat( Object.values( candidates.max ) )
         .filter( ( v, i, a ) => a.indexOf( v ) === i );
-
 }
 
 async function invoke( callback ) {
-
     let result = undefined, error = undefined;
     try { result = await callback() } catch ( e ) { error = e }
 
     return [ result, error ];
-
 }
 
 async function build() {
-
     console.debug( 'Webpack building.' );
-    return new Promise( async ( resolve, reject ) => {
 
+    return new Promise( async ( resolve, reject ) => {
         const p = spawn( 'win32' === process.platform ? 'npm.cmd' : 'npm', [ 'run', 'build' ], { cwd: __dirname } );
         p.on( 'close', e => 0 < e ? reject() : resolve() );
-
     } );
-
 }
 
 async function ws( callback ) {
-
     console.debug( 'Starting web-server.' );
-    return new Promise( async ( resolve, reject ) => {
 
+    return new Promise( async ( resolve, reject ) => {
         let started = false, stderr = '', result = undefined, error = undefined;
 
         const p = spawn( 'win32' === process.platform ? 'npx.cmd' : 'npx', [ 'http-server' ], {
-            cwd: path.resolve( __dirname, 'dist' ) } );
+            cwd: path.resolve( __dirname, 'dist' ),
+        } );
 
         const g = async data => {
-
             if ( started || -1 === data.indexOf( 'to stop the server' ) ) {
-
                 return;
-
             }
 
             started = true;
@@ -134,52 +117,41 @@ async function ws( callback ) {
 
             console.debug( 'Stopping web-server.' );
             kill( p.pid );
-
         };
 
         p.stdout.on( 'data', g );
         p.stderr.on( 'data', data => { stderr += data; g( data ) } );
 
         p.on( 'close', () => ! started ? reject( stderr ) : error ? reject( error ) : resolve( result ) );
-
     } );
-
 }
 
 async function bs( callback ) {
-
     console.debug( 'Starting browserstack.' );
+
     return new Promise( async ( resolve, reject ) => {
-
         const bs = new browserstack.Local();
+
         bs.start( { forceLocal: true }, async e => {
-
             if ( e ) {
-
                 return reject( e );
-
             }
 
             if ( ! bs.isRunning() ) {
-
                 return reject( 'BrowserStack Local is not running.' );
-
             }
 
             let [ result, error ] = await invoke( callback );
 
             console.debug( 'Stopping browserstack.' );
             bs.stop( () => error ? reject( error ) : resolve( result ) );
-
         } );
-
     } );
-
 }
 
 async function wd( caps, callback ) {
-
     console.debug( `Starting ${ caps.browserName }@${ caps.browserVersion }.` );
+
     const p = new webdriver.Builder()
         .usingServer( 'https://hub-cloud.browserstack.com/wd/hub' )
         .withCapabilities( Object.assign( {
@@ -201,26 +173,20 @@ async function wd( caps, callback ) {
     await p.quit();
 
     if ( error ) {
-
         throw error;
-
     }
 
     return result;
-
 }
 
 ( async () => {
-
     try {
-
-        rimraf.sync( path.resolve( __dirname, 'dist' ) );
+        rimraf.sync( path.resolve( __dirname, 'dist' ) + '/*' );
 
         await copy( path.resolve( __dirname, 'static' ), path.resolve( __dirname, 'dist' ) );
         await build();
 
         await ws( () => bs( async () => {
-
             await batch( ( await capabilities() ).map( cap => async () => await wd( cap, async driver => {
 
                 await driver.manage().setTimeouts( { script: 300000, pageLoad: 300000, implicit: 300000 } );
@@ -234,33 +200,22 @@ async function wd( caps, callback ) {
 
                 console.log( `  ${ cap.browserName }@${ cap.browserVersion }: ${ ok ? 'SUCCESS' : 'FAILURE' }\n` );
                 for ( const [ group, results ] of Object.entries( stats ) ) {
-
                     console.log( `    ${ group }` );
+
                     for ( const line of results ) {
-
                         console.log( `      ${ line }` );
-
                     }
-
                 }
 
                 console.log( '' );
 
                 if ( ! ok ) {
-
                     throw 'Tests failed.';
-
                 }
-
             } ) ) );
-
         } ) );
-
     } catch ( e ) {
-
         console.error( e );
         process.exit( 1 );
-
     }
-
 } )();
